8f98dfdccf1463b9fcc9f1a79aa4b4ae616020e8
Bruno <b.manchinelli@gmail.com>
2021-01-04 17:48:43 -0300

    remove explicit type specifications

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -2,28 +2,32 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
-    var tim:String
-    tim = "Tim Buchalka"
+    val jim = "James T Kirk"
+    val tim = "Tim Buchalka"
     println(tim)
 
-    var timsWeeklySalary:Int = 32
-    var timsMonthlySalary: Int = timsWeeklySalary * 4
+    val timsWeeklySalary = 32
+    val timsMonthlySalary: Int = timsWeeklySalary * 4
 
-    println(timsWeeklySalary)
-    println(timsMonthlySalary)
-
-    println()
-
-    val apples: Int = 6
-    val organges: Int = 5
-    var fruit: Int = apples + organges
-    println(fruit)
-
-    println(apples / 4)
+    println("Tim earns $$timsWeeklySalary by week")
+    println("Tim earns $$timsMonthlySalary by month")
 
     println()
 
-    val weeks: Int = 130
+    val apples = 6
+    val organges = 5
+    val fruit: Int = apples + organges
+    println("Tim has $fruit apples and/or oranges")
+
+    println("Aquarter of the apples is $apples / 4")
+
+    println()
+    val weeks = 234
     val years: Double = weeks / 52.0
-    println(years)
+    println("$weeks weeks is $years years")
+
+
+    println("My name is "+ tim)
+    println("Jim is $jim")
+
 }
